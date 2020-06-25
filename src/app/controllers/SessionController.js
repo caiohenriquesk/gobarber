57484350db63a4b0622907c941ef6438f84e0dc3
caiohenriquesk <caiohenriquesk@gmail.com>
2020-06-25 01:06:10 -0300

    Envio de arquivos: configuração Multer

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -7,9 +7,7 @@ import authContig from '../../config/auth';
 class SessionController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required(),
+      email: Yup.string().email().required(),
       password: Yup.string().required(),
     });
 
